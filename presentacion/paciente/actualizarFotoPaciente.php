1b3996ded69131da1c41bafed3805de29e1edd7c
yooo <brayanguitar000@gmail.com>
2019-10-29 00:22:00 -0500

    arreglar actualizar estado solo sirve para id=5 y enviar idpaciente con paremetros

--- presentacion/paciente/actualizarFotoPaciente.php
+++ presentacion/paciente/actualizarFotoPaciente.php
@@ -6,46 +6,6 @@ $paciente -> consultar();
 $bandera = true;
 $mensaje = "";
 
-if(isset($_POST["actualizar"])){
-
-	$nombre = $_FILES['foto']['name'];
-    $tama�o = $_FILES['foto']['size'];
-    $tipo = $_FILES['foto']['type'];
-    
-    if($tama�o <= 1000000)
-    {
-    	if(strpos($tipo, "jpg") || strpos($tipo, "jpeg") || strpos($tipo, "png"))
-    	{
-    		$paciente = new Paciente($_GET["idPaciente"]);
-    		$paciente -> consultarFoto();
-    		
-    		
-    		if(is_file("fotos/".$paciente -> getFoto()) && $paciente -> getFoto()!="default.png")
-    		{
-    			unlink("fotos/".$paciente -> getFoto());	
-    		}	
-    		 
-    		$tipoI = explode('/', $tipo); //separar cadena cuado encuentre un /. Metodo para obtener tipo de imagen
-
-			$destino = $_SERVER['DOCUMENT_ROOT'] . '/ipsud_Yo/fotos/';
-			move_uploaded_file($_FILES['foto']['tmp_name'], $destino . date("jnYhis") . $_GET['idPaciente'] . '.' . $tipoI[1]);
-			 // Se envia al servidor con el nombre modificada en el idPaciente y el tipo de imagen
-
-    		$paciente = new Paciente($_GET["idPaciente"], "", "", "", "", "", "", "", "", date("jnYhis") . $_GET['idPaciente'].'.'.$tipoI[1]);
-    		$paciente -> actualizarFoto();
-            $mensaje = 'Foto del paciente actualizada exitosamente.';
-    	}else{
-            $mensaje = 'Formato Invalido.';
-    		?>
-			<!-- <div class="alert alert-danger" role="alert">Formato Invalido.</div> -->
-    		<?php
-    		$bandera = false;
-    	}
-    }else{
-        $mensaje = 'Tama&ntildeo de imagen Invalido.';
-		//<div class="alert alert-danger" role="alert">Tama&ntildeo de imagen Invalido.</div> -->
-    }
-}
     include 'presentacion/menuAdministrador.php';
 ?>
 	<div class="container">
@@ -55,23 +15,12 @@ if(isset($_POST["actualizar"])){
 				<div class="card">
 					<div class="card-header bg-primary text-white">Actualizar Foto Paciente</div>
 					<div class="card-body">
-						<?php 
-						if(isset($_POST["actualizar"]) && strpos($mensaje, "exitosamente")){ 
-						
-						echo "<div class='alert alert-success role='alert'> " .
-							  $mensaje . "
-						</div>";						
-						 } else if(isset($_POST["actualizar"])) { 
-                            
-                        echo "<div class='alert alert-danger' role='alert'> " .
-                              $mensaje  . "
-                        </div>";
-                            } ?>
-						<form action=<?php echo "index.php?pid=" . base64_encode("presentacion/paciente/actualizarFotoPaciente.php")."&idPaciente=".$_GET["idPaciente"] ?> method="post" enctype="multipart/form-data">
+						<div id="mensaje"></div>
+						<form id="actualizarFoto" method="post" enctype="multipart/form-data">
 							<div class="form-group">
 								<input type="file" name="foto" class="form-control" placeholder="Foto" required="required" >
 							</div>
-							<button type="submit" name="actualizar" class="btn btn-primary">Actualizar</button>
+							<input type="button" onclick="actualizarFoto();" name="actualizar" class="btn btn-primary" value="Actulizar">
 						</form>
 					</div>
 				</div>
@@ -79,4 +28,29 @@ if(isset($_POST["actualizar"])){
 
 		</div>
 
-	</div>	
+	</div>		
+
+
+<script>
+function actualizarFoto(){
+	var parametros = new FormData($("#actualizarFoto")[0]);
+
+	<?php  echo "var ruta = \"index.php?pid=" . base64_encode("presentacion/paciente/actualizarFotoPacienteAjax.php") . "\";\n"; ?>
+
+	$.ajax({
+		data: parametros,
+		url: ruta,
+		type: "POST",
+		contentType: false,
+		processData: false,
+		beforesend: function(){
+
+		},
+		success: function(response){
+			$("#mensaje").html(response);
+		}
+	});
+
+}
+
+</script>
---
+++ presentacion/paciente/actualizarFotoPacienteAjax.php
@@ -0,0 +1,46 @@
+<?php
+
+$administrador = new Administrador($_SESSION['id']);
+$administrador->consultar();
+include 'presentacion/menuAdministrador.php';
+//$nombre = $_FILES['foto']['name'];
+//$tamaño = $_FILES['foto']['size'];
+//$tipo = $_FILES['foto']['type'];
+
+$foto = $_FILES['foto'];
+
+if($foto["size"] <= 1000000)
+{
+    if($foto["type"]  == "image/jpg" or $foto["type"]  == "image/png" or $foto["type"]  == "image/jpeg" /*strpos($tipo, "jpg") || strpos($tipo, "jpeg") || strpos($tipo, "png")*/)
+    {
+        $paciente = new Paciente(1);
+        $paciente -> consultarFoto();
+        
+        
+        if(is_file("fotos/".$paciente -> getFoto()) && $paciente -> getFoto()!="default.png")
+        {
+            unlink("fotos/".$paciente -> getFoto());	
+        }	
+         
+        //$tipoI = explode('/', $tipo); //separar cadena cuado encuentre un /. Metodo para obtener tipo de imagen
+
+        $destino = $_SERVER['DOCUMENT_ROOT'] . '/ipsud_Yo/fotos/';
+        move_uploaded_file($foto['tmp_name'], $destino . date("jnYhis") . '.jpg');
+         // Se envia al servidor con el nombre modificada en el idPaciente y el tipo de imagen
+
+        $paciente = new Paciente(1, "", "", "", "", "", "", "", "", date("jnYhis") . '.jpg');
+        $paciente -> actualizarFoto();
+        echo "<div class='alert alert-success' role='alert'> Foto del paciente actualizada exitosamente. </div>";
+    }else{
+        echo "<div class='alert alert-danger' role='alert'> Formato Invalido. </div>";
+        ?>
+        <!-- <div class="alert alert-danger" role="alert">Formato Invalido.</div> -->
+        <?php
+        $bandera = false;
+    }
+}else{
+    echo "<div class='alert alert-danger' role='alert'> Tama&ntildeo de imagen Invalido. </div>";
+    //<div class="alert alert-danger" role="alert">Tama&ntildeo de imagen Invalido.</div> -->
+}
+
+?>
