ef2b4bf5104ea87b539bb2c79e5af2459bfb6168
yooo <brayanguitar000@gmail.com>
2019-10-30 13:06:48 -0500

    version 8.1

--- presentacion/menuAdministrador.php
+++ presentacion/menuAdministrador.php
@@ -1,40 +1,35 @@
 <head>
-<link href="estilos.css" rel="stylesheet" type="text/css"/>
+	<link href="presentacion/estilos.css" rel="stylesheet" type="text/css" />
 </head>
 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<a class="navbar-brand"
-		href="index.php?pid=<?php echo base64_encode("presentacion/sesionAdministrador.php")?>"><i
-		class="fas fa-home"></i></a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse"
-		data-target="#navbarSupportedContent"
-		aria-controls="navbarSupportedContent" aria-expanded="false"
-		aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="login" >
-		<ul >
-			<li><a href="#"> Consultar </a>
+	<div class="d-flex">
+		<a class="navbar-brand" href="index.php?pid=<?php echo base64_encode("presentacion/sesionAdministrador.php") ?>"><i class="fas fa-home"></i></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
+		<div class="login" style="z-index: 2;" id="con">
 			<ul>
-                                <li>
-                                    <a class="ani" href="#">
-                                        Administrador
-                                    </a>
-                                </li>
-                                <li><a class="an" href="index.php?pid=<?php echo base64_encode("presentacion/paciente/consultarPaciente.php")?>">Paciente</a>
-								</li>
-                                <li><a class="an" href="#">Medico</a></li>
-                                <li class="nav-item"><a class="nav-link" href="index.php">Salida</a>
-			</li>
-                            </ul>
+				<li><a href="#"> Consultar </a>
 
-
-				
-		</ul>
-		<span class="navbar-text">
-      Administrador: <?php echo $administrador -> getNombre() . " " . $administrador -> getApellido() ?> 
-    </span>
+					<ul>
+						<li>
+							<a class="ani" href="#">
+								Administrador
+							</a>
+						</li>
+						<li><a class="an" href="index.php?pid=<?php echo base64_encode("presentacion/paciente/consultarPaciente.php") ?>">Paciente</a>
+						</li>
+						<li><a class="an" href="#">Medico</a></li>
+					</ul>
+			</ul>
+		</div>
+		<div style="padding: 5px;"><a style="color: #8F8E8E;" href="index.php">Salir</a></div>
 	</div>
+
+	<span class="navbar-text" style="margin-left: 850px;">
+		Administrador: <?php echo $administrador->getNombre() . " " . $administrador->getApellido() ?>
+	</span>
 </nav>
--- presentacion/sesionAdministrador.php
+++ presentacion/sesionAdministrador.php
@@ -17,3 +17,5 @@ include 'presentacion/menuAdministrador.php';
 		</div>
 	</div>
 </div>
+
+</main>
